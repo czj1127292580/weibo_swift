b427e6d30068d6afbb2d74bac50943f5780a2376
czj1127292580 <1127292580@qq.com>
2016-08-15 23:00:12 +0800

    1、搭建基本结构

--- WeiBo_Swift/WeiBo_Swift/Class/Main/MainViewController.swift
+++ WeiBo_Swift/WeiBo_Swift/Class/Main/MainViewController.swift
@@ -12,8 +12,47 @@ class MainViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tabBar.tintColor = UIColor.orangeColor()
 
-        // Do any additional setup after loading the view.
+//        let home = HomeTableViewController()
+//        home.tabBarItem.image = UIImage(named: "tabbar_home")
+//        home.tabBarItem.selectedImage = UIImage(named: "tabbar_home_highlighted")
+//        home.tabBarItem.title = "首页"
+//        home.title = "首页"
+//        let nav = UINavigationController()
+//        nav.addChildViewController(home)
+//        
+//        addChildViewController(nav)
+        
+        addChildViewController(HomeTableViewController(), title: "首页", imageName: "tabbar_home")
+        addChildViewController(MessageTableViewController(), title: "消息", imageName: "tabbar_message_center")
+
+        addChildViewController(ProfileTableViewController(), title: "我", imageName: "tabbar_profile")
+
+        addChildViewController(DiscoverTableViewController(), title: "广场", imageName: "tabbar_discover")
+
+        
+    }
+    
+    /**
+     初始化子控制器
+     
+     - parameter childController: 需要初始化的子控制器
+     - parameter title:           子控制器的标题
+     - parameter imageName:       子控制器的图片
+     */
+    private func addChildViewController(childController: UIViewController, title:String, imageName:String) {
+        
+        childController.tabBarItem.image = UIImage(named: imageName)
+        childController.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
+        childController.title = title
+        
+        let nav = UINavigationController()
+        nav.addChildViewController(childController)
+        
+        addChildViewController(nav)
+    
     }
 
 }
