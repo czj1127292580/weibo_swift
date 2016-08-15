e8e7d9ecbc56656f2aa86b8dda7caff2ad9819b4
czj1127292580 <1127292580@qq.com>
2016-08-16 00:17:33 +0800

    1、添加加号按钮

--- WeiBo_Swift/WeiBo_Swift/Class/Main/MainViewController.swift
+++ WeiBo_Swift/WeiBo_Swift/Class/Main/MainViewController.swift
@@ -15,6 +15,31 @@ class MainViewController: UITabBarController {
         
         tabBar.tintColor = UIColor.orangeColor()
         
+        
+        addChildViewControllers()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        /**
+          添加加号按钮
+         */
+        setUpComposeBtn()
+        
+    }
+    
+    private func setUpComposeBtn(){
+        
+        tabBar.addSubview(composeBtn)
+        
+        let width = UIScreen.mainScreen().bounds.size.width / CGFloat(viewControllers!.count)
+        let rect = CGRect(x: 0, y: 0, width: width, height: 49)
+        composeBtn.frame = CGRectOffset(rect, 2 * width, 0)
+        
+    }
+    
+    private func addChildViewControllers() {
         let path = NSBundle.mainBundle().pathForResource("MainVCSettings.json", ofType: nil)
         if let jsonPath = path{
             let jsonData = NSData(contentsOfFile: jsonPath)
@@ -31,7 +56,7 @@ class MainViewController: UITabBarController {
                 for dict in dictArr as! [[String: String]] {
                     addChildViewController(dict["vcName"]!, title: dict["title"]!, imageName: dict["imageName"]!)
                 }
-
+                
             }catch{
                 // 发生异常之后会执行的代码
                 print(error)
@@ -43,20 +68,17 @@ class MainViewController: UITabBarController {
                 addChildViewController("ProfileTableViewController", title: "我", imageName: "tabbar_profile")
                 
                 addChildViewController("DiscoverTableViewController", title: "广场", imageName: "tabbar_discover")
-
+                
             }
-            
         }
         
         
-//        addChildViewController("HomeTableViewController", title: "首页", imageName: "tabbar_home")
-//        addChildViewController("MessageTableViewController", title: "消息", imageName: "tabbar_message_center")
-//
-//        addChildViewController("ProfileTableViewController", title: "我", imageName: "tabbar_profile")
-//
-//        addChildViewController("DiscoverTableViewController", title: "广场", imageName: "tabbar_discover")
-
-        
+        //        addChildViewController("HomeTableViewController", title: "首页", imageName: "tabbar_home")
+        //        addChildViewController("MessageTableViewController", title: "消息", imageName: "tabbar_message_center")
+        //
+        //        addChildViewController("ProfileTableViewController", title: "我", imageName: "tabbar_profile")
+        //
+        //        addChildViewController("DiscoverTableViewController", title: "广场", imageName: "tabbar_discover")
     }
     
     /**
@@ -76,11 +98,6 @@ class MainViewController: UITabBarController {
         let vcCls = cls as! UIViewController.Type
         let vc = vcCls.init()
         
-        
-        
-        
-        
-        
         vc.tabBarItem.image = UIImage(named: imageName)
         vc.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
         vc.title = title
@@ -90,6 +107,28 @@ class MainViewController: UITabBarController {
         
         addChildViewController(nav)
     
+    }
+    
+    // MARK: - 懒加载
+    private lazy var composeBtn: UIButton = {
+        
+        let btn = UIButton()
+        btn.setImage(UIImage(named: "tabbar_compose_icon_add"), forState: UIControlState.Normal)
+        btn.setImage(UIImage(named:"tabbar_compose_icon_add_highlighted"), forState: UIControlState.Highlighted)
+        btn.setBackgroundImage(UIImage(named:"tabbar_compose_button"), forState: UIControlState.Normal)
+        btn.setBackgroundImage(UIImage(named: "tabbar_compose_button_highlighted"), forState: UIControlState.Highlighted)
+        
+        btn.addTarget(self, action: "composeBtnClick", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        return btn
+        
+    }()
+    
+    /**
+     监听按钮的方法不能是私有方法
+     */
+    func composeBtnClick() {
+        
     }
 
 }
