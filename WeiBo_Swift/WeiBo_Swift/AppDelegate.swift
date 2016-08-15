1fd06de93e8b13859d57d3e86ae40b7506224797
czj1127292580 <1127292580@qq.com>
2016-08-15 22:39:55 +0800

    1、初始化项目，搭建基本结构

---
+++ WeiBo_Swift/WeiBo_Swift/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  WeiBo_Swift
+//
+//  Created by 岑志军 on 16/8/15.
+//  Copyright © 2016年 cen. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.backgroundColor = UIColor.whiteColor()
+        window?.rootViewController = MainViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+}
+
---
+++ WeiBo_Swift/WeiBo_Swift/Class/Main/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  WeiBo_Swift
+//
+//  Created by 岑志军 on 16/8/15.
+//  Copyright © 2016年 cen. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
